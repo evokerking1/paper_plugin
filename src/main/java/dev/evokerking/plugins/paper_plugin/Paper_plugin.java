dad5fa996b95a3ff61034c80211e8a2c1838dde3
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2024-04-05 07:54:59 -0400

    Initial commit

---
+++ src/main/java/dev/evokerking/plugins/paper_plugin/Paper_plugin.java
@@ -0,0 +1,17 @@
+package dev.evokerking.plugins.paper_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Paper_plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
